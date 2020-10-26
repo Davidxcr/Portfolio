a7ba40bd0195202cb72c4e6d9bb077d070fb53ac
xcript1 <o_olatunji@mail.harper.edu>
2020-10-26 14:40:13 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,8 @@ sr.reveal('.home__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('about_logo',{});
-sr.reveal('.about__img',{}); 
+sr.reveal('.about__img', {});
+sr.reveal('.about_logo',{});
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
