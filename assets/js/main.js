23cb1f3990d71d623335e975821eb1945013519c
xcript1 <o_olatunji@mail.harper.edu>
2020-12-16 17:55:31 -0600

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__img',{delay: 300}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
@@ -44,14 +44,14 @@ sr.reveal('.about__img',{});
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+/*SCROLL TECHNOLOGIES*/
+sr.reveal('.technologies__subtitle',{}); 
+sr.reveal('.technologies__text',{}); 
+sr.reveal('.technologies__data',{interval: 200}); 
+sr.reveal('.technologies__img',{delay: 600});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.portfolio__img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__img',{interval: 200}); 
