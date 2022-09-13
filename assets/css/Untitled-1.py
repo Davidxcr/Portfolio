04e07f995f88e3ef3bf9cd41998f467e2c6b8292
Davidxcr <vodolatunji@gmail.com>
2022-09-13 14:58:52 -0500

    testing new update

---
+++ assets/css/Untitled-1.py
@@ -0,0 +1,9 @@
+
+
+def battery(terminal):
+    print(terminal)
+    print(a)
+    a = "negative"
+
+a = "positive"
+battery(a)
---
+++ assets/css/uniii.js
@@ -0,0 +1,5 @@
+var I
+var y = 6
+var x = "oooo"
+for (i = 0; i < x.length; i++) { y += i }
+console.log(y)
